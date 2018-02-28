4fcd40531d3703d615bdc5b55fc43e75c37bc4df
sugestio <github@sugestio.com>
2018-02-28 16:37:56 +0100

    updated version number and requirements

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@
 from setuptools import setup, find_packages
 
 setup(name="sugestio",
-      version="0.4",
+      version="0.5",
       description="Sugestio library for python",
       url="http://github.com/sugestio/sugestio-python",
       packages=find_packages(),
-      install_requires=['oauth2','urllib','csv','simplejson'],
+      install_requires=['oauth2','urllib','json','simplejson'],
       license = "MIT License",
       keywords="sugestio recommendations library")
